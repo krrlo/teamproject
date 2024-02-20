89dca915484b9cc4a968282af066c030d7215aaa
두웅이 <148922859+krrlo@users.noreply.github.com>
2024-02-20 11:15:32 +0900

    0220 서버

--- local/server.js
+++ local/server.js
@@ -1,6 +1,6 @@
 //웹소켓서버
 const express = require("express");
-const cors = require("cors");
+//const cors = require("cors");
 const app = express();
 // HTTP 서버(server)를 생성
 const server = require("http").Server(app);
@@ -13,25 +13,9 @@ const io = require("socket.io")(server, {
   },
 });
 
-//const { v4: uuidV4 } = require("uuid");
 
-//console.log(`${uuidV4()}`);
 
-//app.use(cors());
-//app.set("view engine", "ejs");
-//app.use(express.static("public"));
 
-// app.get("/", (req, res) => {
-//   res.redirect(`/${uuidV4()}`);
-// });
-
-// 방 고유아이디를 생성한다음 해당 id를 사용하여 방에 참여할수 있도록 함
-// roomId란 이름으로 렌더링할 HTML 템플릿에 방 ID 전달하는 역할
-// room ejs로 보냄
-// app.get("/:room", (req, res) => {
-//   res.render("room", { roomId: req.params.room });
-//   //res.json({ roomId: req.params.room });
-// });
 
 //서버와 클라이언트가 연결되면 핸들러 실행
 io.on("connection", (socket) => {
@@ -40,16 +24,16 @@ io.on("connection", (socket) => {
 
   socket.on("join-room", (roomId, userId) => {
     console.log("룸아이디", roomId);
+    console.log("유저아이디", userId);
     //서버는 해당 사용자를  방에 조인 시킴
     socket.join(roomId);
     //해당방에 속한 다른 사용자들에게 새로운 사용자가 방에 연결되었음을 알리기 위해 "user-connected"이벤트를 브로드캐스트 함
     socket.to(roomId).broadcast.emit("user-connected", userId);
-    console.log("브로드캐스트");
 
     socket.on("disconnect", () => {
+      console.log("연결해제");
       //이때 서버는 해당 방에서 사용자가 연결을 해제했음을 다른 사용자들에게 알리기 위해 "user-disconnected" 이벤트를 브로드캐스트
       socket.to(roomId).broadcast.emit("user-disconnected", userId);
-      console.log("연결해제");
     });
   });
 });
